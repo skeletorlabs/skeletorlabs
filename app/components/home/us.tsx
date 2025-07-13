7d8d183026da180024f8a60f7b7d59b6690f2fd4
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2025-07-13 20:58:41 -0300

    update talk text size

--- app/components/home/us.tsx
+++ app/components/home/us.tsx
@@ -61,7 +61,7 @@ export default function Us() {
           ))}
         </div>
       </div>
-      <p className="text-[120px] text-center xl:text-[180px] px-4 xl:px-10 pt-6 pb-16 font-bold leading-tight xl:leading-none">
+      <p className="text-[110px] text-center xl:text-[180px] px-4 xl:px-10 pt-6 pb-16 font-bold leading-tight xl:leading-none">
         <button
           onClick={() => setTalkIsOpen(true)}
           className="transition-all duration-200 hover:scale-[1.02] hover:text-violet-200 tracking-wider"
